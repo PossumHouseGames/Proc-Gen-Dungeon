fb9186dcf6c4b15b0acc8ec555e721a18fce92d3
lestherreynoso <lhreynos@gmail.com>
2024-03-03 15:19:10 -0500

    Tweening movement and rotation

--- scripts/player/PlayerController.cs
+++ scripts/player/PlayerController.cs
@@ -11,7 +11,8 @@ public partial class PlayerController : Node3D
 	private GridObject _myGridObject;
 
 	private Camera3D _myCam;
-	
+	private Tween _tween;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -32,33 +33,63 @@ public partial class PlayerController : Node3D
 		if (_inputHandler.RotateLeft)
 		{
 			_myGridObject.RotateLeft();
-			RotationDegrees = Vector3.Up * _myGridObject.rotation;
-			GameDebug.Log(_myGridObject.ToString());
-			GameDebug.Log($"Player at {_myGridObject.position}, facing {_myGridObject.faceDirection}");
+			// RotationDegrees = Vector3.Up * _myGridObject.rotation;
+
+			SyncRotation();
 		}
 		if (_inputHandler.RotateRight)
 		{
 			_myGridObject.RotateRight();
-			RotationDegrees = Vector3.Up * _myGridObject.rotation;
-			GameDebug.Log(_myGridObject.ToString());
-			GameDebug.Log($"Player at {_myGridObject.position}, facing {_myGridObject.faceDirection}");
+			//RotationDegrees = Vector3.Up * _myGridObject.rotation;
+			
+			SyncRotation();
 		}
 
 		if (_inputHandler.MoveForward)
 		{
 			_myGridObject.MoveForward();
-			Position = new Vector3(-_myGridObject.position.X, 0, _myGridObject.position.Y);
-			// Tween tween = CreateTween();
-			// tween.TweenProperty(this, "position", _myGridObject.position, 0.2f);
-			GameDebug.Log($"Player at {_myGridObject.position}, facing {_myGridObject.faceDirection}");
+			// Position = new Vector3(-_myGridObject.position.X, 0, _myGridObject.position.Y); 
+
+			SyncPosition();
 		}
 		if (_inputHandler.MoveBack)
 		{
 			_myGridObject.MoveBack();
-			Position = new Vector3(-_myGridObject.position.X, 0, _myGridObject.position.Y);
-			// Tween tween = CreateTween();
-			// tween.TweenProperty(this, "position", _myGridObject.position, 0.2f);
-			GameDebug.Log($"Player at {_myGridObject.position}, facing {_myGridObject.faceDirection}");
+			// Position = new Vector3(-_myGridObject.position.X, 0, _myGridObject.position.Y); 
+			
+			SyncPosition();
 		}
+	}
+
+	private void SyncPosition()
+	{
+		var targetPos = new Vector3(-_myGridObject.position.X, 0, _myGridObject.position.Y);
+		_tween = CreateTween();
+		_tween.TweenProperty(this, "position", targetPos, 0.2f);
+		GameDebug.Log($"Player at {_myGridObject.position}, facing {_myGridObject.faceDirection}");
+	}
+
+	private void SyncRotation()
+	{
+		var targetRot = Vector3.Up * _myGridObject.rotation;
+		SetRotationBeforeTween();
+
+		
+		_tween = CreateTween();
+		_tween.TweenProperty(this, "rotation_degrees", targetRot, 0.2f);
+
+		GameDebug.Log(_myGridObject.ToString());
+		GameDebug.Log($"Player at {_myGridObject.position}, facing {_myGridObject.faceDirection}");
+	}
+
+	private void SetRotationBeforeTween()
+	{
+		RotationDegrees = _myGridObject.rotation switch
+		{
+			270 when Math.Abs(RotationDegrees.Y - 0) < 0.002f => Vector3.Up * 360,
+			90 when Math.Abs(RotationDegrees.Y - 360) < 0.002f => Vector3.Zero,
+			0 when Mathf.Abs(RotationDegrees.Y - 270) < 0.002f => Vector3.Up * -90,
+			_ => RotationDegrees
+		};
 	}
 }
